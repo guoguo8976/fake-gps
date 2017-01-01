e32f930149ec7233e5bfd4c742ad656a47f4a749
yguo92 <yguo92@bloomberg.net>
2017-01-01 14:33:46 -0500

    Initial Commit

---
+++ gps fake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gps fake
+//
+//  Created by yguo92 on 2017/1/1.
+//  Copyright © 2017年 guoguo8976. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
